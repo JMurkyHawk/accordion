c2a6c8d2a20fe5d080a5cd6145bef5b258cf1c85
JMurkyHawk <jmiaguard-github@yahoo.com>
2024-01-14 10:31:26 -0600

    Add navigation component unit tests

--- src/app/components/j-murky-hawk-navigation/j-murky-hawk-navigation.component.spec.ts
+++ src/app/components/j-murky-hawk-navigation/j-murky-hawk-navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { Router, RouterLinkActive } from '@angular/router';
 import { By } from '@angular/platform-browser';
@@ -42,9 +42,22 @@ describe('JMurkyHawkNavigationComponent', () => {
         component = fixture.componentInstance;
         debugElement = fixture.debugElement;
         component.navItems = links;
+        component.linkStyle = 'text';
         linkButton = fixture.nativeElement.querySelector('a');
         fixture.detectChanges();
     })
+
+    const createScrollToElement = (element: string, scrollToId: string) => {
+        const checkElement = document.getElementById(scrollToId);
+        
+        if (checkElement) {
+            checkElement.remove();
+        }
+
+        document.body
+        .appendChild(document.createElement(element))
+        .setAttribute('id', scrollToId);
+    }
 
     it('should create', () => {
         expect(component).toBeTruthy();
@@ -53,12 +66,76 @@ describe('JMurkyHawkNavigationComponent', () => {
     it('should execute linkClick() method when <a> link is clicked', () => {
 
         spyOn(component, 'linkClick');
-        component.linkType = 'button';
-        fixture.detectChanges();
         let btn = fixture.debugElement.query(By.css('a'));
+        expect(btn).toBeTruthy;
+        
         btn.triggerEventHandler('click', null);
         fixture.detectChanges();
         expect(component.linkClick).toHaveBeenCalled();
+    });
+
+    it('if linkScrollTo="true", <a> link click should scroll default id element into view', fakeAsync(() => {
+        createScrollToElement('div', 'mainNavBar');
+        component.linkScrollTo = true;
+        component.linkScrollToId = 'mainNavBar';
+        fixture.detectChanges();
+
+        const doc = fixture.nativeElement.ownerDocument;
+        const getScrollToElement = doc.getElementById(component.linkScrollToId);
+        expect(getScrollToElement).toBeTruthy;
+        
+        const btn = fixture.debugElement.query(By.css('a'));
+        expect(btn).toBeTruthy;
+
+        spyOn(getScrollToElement, 'scrollIntoView').and.callThrough();
+        btn.triggerEventHandler('click', null);
+        tick(component.linkScrollDelay);
+        expect(getScrollToElement.scrollIntoView).toHaveBeenCalled();
+    }));
+
+    it('checkScrollToIdValue() should return the PROVIDED id value if element with that id exists in DOM', () => {
+        const scrollToId = 'mainNavBar';
+        createScrollToElement('div', 'mainNavBar');
+
+        component.linkScrollTo = true;
+        component.linkScrollToId = scrollToId;
+        fixture.detectChanges();
+        expect(document.getElementById(component.linkScrollToId)).toBeTruthy;
+        expect(component.checkScrollToIdValue(component.linkScrollToId)).toBe(scrollToId);
+    });
+
+    it('checkScrollToIdValue() should return the DEFAULT id value if element with that id does NOT exist in DOM', () => {
+        const scrollToId = 'id-not-in-dom';
+        const defaultIdValue = component.linkScrollToId;
+        createScrollToElement('div', 'mainNavBar');
+
+        component.linkScrollTo = true;
+        component.linkScrollToId = scrollToId;
+        fixture.detectChanges();
+        expect(document.getElementById(component.linkScrollToId)).toBeTruthy;
+        expect(component.checkScrollToIdValue(component.linkScrollToId)).toBe(defaultIdValue);
+    });
+
+    it('checkScrollToIdValue() should set id element tabindex="0" if element is NOT <a> or <button>', () => {
+        const scrollToId = 'mainNavBar';
+        createScrollToElement('div', 'mainNavBar');
+
+        component.linkScrollTo = true;
+        component.linkScrollToId = scrollToId;
+        fixture.detectChanges();
+        expect(document.getElementById(component.linkScrollToId)).toBeTruthy;
+        expect(document.getElementById(component.linkScrollToId)?.getAttribute('tabindex')).toBe('0');
+    });
+
+    it('checkScrollToIdValue() should NOT add tabindex if element is <a> or <button>', () => {
+        const scrollToId = 'mainNavBar';
+        createScrollToElement('a', 'mainNavBar');
+
+        component.linkScrollTo = true;
+        component.linkScrollToId = scrollToId;
+        fixture.detectChanges();
+        expect(document.getElementById(component.linkScrollToId)).toBeTruthy;
+        expect(document.getElementById(component.linkScrollToId)?.hasAttribute('tabindex')).toBe(false);
     });
 
 });
